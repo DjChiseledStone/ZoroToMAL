ee73152e96eb24fadd1d65a09d0be580f31e86a4
DjChiseledStone <djchiseledstone@gmail.com>
2024-03-13 02:35:51 +1000

    Creation

---
+++ ZoroToMAL.py
@@ -0,0 +1,36 @@
+file = open("export.txt", "r")
+importFile = open("import.xml", "w")
+status = ""
+animeCode = ""
+
+importFile.write("""<?xml version="1.0" encoding="UTF-8" ?>		
+		<myanimelist>
+			<myinfo>
+				<user_export_type>1</user_export_type>
+			</myinfo>""")
+for line in file:
+    line = line.strip()
+    sep = line.split("/")
+    if sep[-1] == "# Plan to watch":
+        status = "Plan to Watch"
+    elif sep[-1] == "# Completed":
+        status = "Completed"
+    elif sep[-1] == "# Watching":
+        status = "Watching"
+    elif sep[-1] == "# On-Hold":
+        status = "On-Hold"
+    elif sep[-1] == "# Dropped":
+        status = "Dropped"
+    else:
+        animeCode = sep[-1]
+        importFile.write(
+                f"""
+                <anime>
+                    <series_animedb_id>{animeCode}</series_animedb_id>
+                    <my_status>{status}</my_status>
+                    <update_on_import>1</update_on_import>
+                </anime>\n"""
+            )
+importFile.write("</myanimelist>")
+file.close()
+importFile.close()
